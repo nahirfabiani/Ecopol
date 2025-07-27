d5411bac98539bef006742003f21f4ef7e076b9a
nahir <nahirfabiani@hotmail.com>
2025-07-27 12:54:22 -0300

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,98 @@
+const navId = document.getElementById("nav_menu"),
+  ToggleBtnId = document.getElementById("toggle_btn"),
+  CloseBtnId = document.getElementById("close_btn");
+
+// ==== SHOW MENU ==== //
+ToggleBtnId.addEventListener("click", () => {
+  navId.classList.add("show");
+});
+
+// ==== HIDE MENU ==== //
+CloseBtnId.addEventListener("click", () => {
+  navId.classList.remove("show");
+});
+
+// ==== Animate on Scroll Initialize  ==== //
+AOS.init();
+
+// ==== GSAP Animations ==== //
+// ==== LOGO  ==== //
+gsap.from(".logo", {
+  opacity: 0,
+  y: -10,
+  delay: 1,
+  duration: 0.5,
+});
+// ==== NAV-MENU ==== //
+gsap.from(".nav_menu_list .nav_menu_item", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+  stagger: 0.3,
+});
+// ==== TOGGLE BTN ==== //
+gsap.from(".toggle_btn", {
+  opacity: 0,
+  y: -10,
+  delay: 1.4,
+  duration: 0.5,
+});
+// ==== MAIN HEADING  ==== //
+gsap.from(".main-heading", {
+  opacity: 0,
+  y: 20,
+  delay: 2.4,
+  duration: 1,
+});
+// ==== INFO TEXT ==== //
+gsap.from(".info-text", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+// ==== CTA BUTTONS ==== //
+gsap.from(".btn_wrapper", {
+  opacity: 0,
+  y: 20,
+  delay: 2.8,
+  duration: 1,
+});
+// ==== TEAM IMAGE ==== //
+gsap.from(".team_img_wrapper img", {
+  opacity: 0,
+  y: 20,
+  delay: 3,
+  duration: 1,
+});
+// Modal de contacto
+const contactBtn = document.querySelector('.view_more_btn');
+const modal = document.getElementById('contactModal');
+const closeModal = document.getElementById('closeModal');
+
+contactBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+// Animación para la sección "nosotros" usando GSAP
+gsap.from("#nosotros", {
+  opacity: 0,
+  y: 60,
+  duration: 1.2,
+  ease: "power2.out",
+  delay: 0.5
+});
+
+AOS.init();
